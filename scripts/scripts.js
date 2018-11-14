e8f1451eb379cd8d63c026a3f04c6ae3095713cb
Wilson <wilsonj806@gmail.com>
2018-11-14 13:28:47 -0500

    Make filter bar responsive
    - Added SCSS so filter bar is responsive on most devices
    - Added JS to implement collapse for the filter bar on mobile

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,32 @@
+// TODO: Add animations in for buttons
+// TODO: Determine way to implement collapse without declaring global variables and document.querySelector
+
 // DOM Element Retrieval
 
 const burger = document.querySelector('div.nav__burger');
-const navList = document.querySelector('.nav__list');
-
-// ideally not require the burger variable in the navCollapse function
-  // implementing that probably requires a bit more scripting
-
+const collapseRow = document.querySelector('.filter-coll');
 
 // Functions
 
-function navCollapse(e){
-  console.log(e);
+function navCollapse(e) {
   if ((e.target.parentElement !== burger) && (e.target !== burger)) return;
-  if (e.target===burger){
+  if (e.target === burger) {
     let targetEle = document.querySelector(e.target.dataset.target);
     targetEle.classList.toggle('js-toggle');
   }
-  else{
+  else {
+    let targetEle = document.querySelector(e.target.parentElement.dataset.target);
+    targetEle.classList.toggle('js-toggle');
+  }
+}
+
+function filterCollapse(e) {
+  if ((e.target.parentElement !== collapseRow) && (e.target !== collapseRow)) return;
+  if (e.target === collapseRow) {
+    let targetEle = document.querySelector(e.target.dataset.target);
+    targetEle.classList.toggle('js-toggle');
+  }
+  else {
     let targetEle = document.querySelector(e.target.parentElement.dataset.target);
     targetEle.classList.toggle('js-toggle');
   }
@@ -25,3 +35,4 @@ function navCollapse(e){
 // Event Listeners
 
 burger.addEventListener('click',navCollapse);
+collapseRow.addEventListener('click', filterCollapse);
